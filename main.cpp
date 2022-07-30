6aaef0dcf580fd034ff46736de4b955f9b9bfda1
dimanchique <ghettorunner@yandex.ru>
2022-07-30 22:34:01 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,44 +1,55 @@
 #include <iostream>
 #include "EntityManager.h"
 #include <chrono>
+#include "string"
+
+void SetInputParams(int &num, int &field_size, float &fov, int &view_distance);
+void LogDuration(std::chrono::high_resolution_clock::time_point t1, std::chrono::high_resolution_clock::time_point t2, const std::string& operation);
 
 int main() {
     EntityManager Manager;
-    int NumOfEntities, FieldSize, FOV, ViewDistance;
+    int NumOfEntities, FieldSize, ViewDistance; float FOV;
+    std::chrono::high_resolution_clock::time_point start_time, stop_time;
 
-    std::cout << "Enter number of entities: "; std::cin >> NumOfEntities; std::cout << std::endl;
+    SetInputParams(NumOfEntities, FieldSize, FOV, ViewDistance);
 
+    start_time = std::chrono::high_resolution_clock::now();
+    Manager.CreateEntities(FieldSize, NumOfEntities, FOV, ViewDistance);
+    stop_time = std::chrono::high_resolution_clock::now();
+    LogDuration(start_time, stop_time, "creation");
+
+    start_time = std::chrono::high_resolution_clock::now();
+    Manager.CalculateVisibleUnits();
+    stop_time = std::chrono::high_resolution_clock::now();
+    LogDuration(start_time, stop_time, "calculation");
+
+    for (auto &Pair : Manager.EntityByID)
+    {
+        auto entity = Pair.second;
+        printf("Unit %d see %d units: { ", entity.ID, entity.VisibleEntitiesIDs.size());
+        for (auto ID : entity.VisibleEntitiesIDs)
+            std::cout << ID << " ";
+        printf("}\n");
+    }
+    return 0;
+}
+
+void SetInputParams(int &num, int &field_size, float &fov, int &view_distance)
+{
+    printf("Enter number of units: "); std::cin >> num; std::cout << std::endl;
     do {
-        std::cout << "Enter field size: "; std::cin >> FieldSize; std::cout << std::endl;
-        if (FieldSize*FieldSize > NumOfEntities)
-            break;
-        std::cout << "Can't fit " << NumOfEntities << " entities in field with size " << FieldSize << "x" << FieldSize << ". Try Again" << std::endl;
+        printf("Enter field size: "); std::cin >> field_size; std::cout << std::endl;
+        if (field_size*field_size > num) break;
+        printf("Can't fit %d units in field with size %dx%d. Try Again!\n", num, field_size, field_size);
     }
     while (true);
 
-    std::cout << "Enter FOV: "; std::cin >> FOV; std::cout << std::endl;
-    std::cout << "Enter view distance: "; std::cin >> ViewDistance; std::cout << std::endl;
+    printf("Enter FOV: "); std::cin >> fov; std::cout << std::endl;
+    printf("Enter view distance: "); std::cin >> view_distance; std::cout << std::endl;
+}
 
-    auto start = std::chrono::high_resolution_clock::now();
-    Manager.CreateEntities(FieldSize, NumOfEntities, FOV, ViewDistance);
-    auto end = std::chrono::high_resolution_clock::now();
-    double time_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); time_taken *= 1e-9;
-    std::cout << "Time taken by creation is : " << std::fixed << time_taken << " sec " << std::endl;
-
-    start = std::chrono::high_resolution_clock::now();
-    Manager.CalculateVisibleUnits();
-    end = std::chrono::high_resolution_clock::now();
-    time_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); time_taken *= 1e-9;
-    std::cout << "Time taken by calculation is : " << std::fixed << time_taken << " sec " << std::endl;
-
-    for (auto Pair : Manager.EntityByID)
-    {
-        auto entity = Pair.second;
-        printf("Entity %d see %d entities: ", entity.ID, entity.VisibleEntitiesIDs.size());
-        std::cout << "{ ";
-        for (auto ID : entity.VisibleEntitiesIDs)
-            std::cout << ID << " ";
-        std::cout << "}" << std::endl;
-    }
-    return 0;
+void LogDuration(std::chrono::high_resolution_clock::time_point t1, std::chrono::high_resolution_clock::time_point t2, const std::string& operation)
+{
+    std::chrono::duration<double, std::milli> ms_double = (t2 - t1)/1000;
+    printf("Time taken by %s is: %f s\n", operation.c_str(), ms_double.count());
 }
